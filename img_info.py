1acfdd521c543a2ce83421981af580976111870c
Abhiek187 <achaudhuri2011@yahoo.com>
2020-02-28 23:57:37 -0500

    Scale printing area

--- img_info.py
+++ img_info.py
@@ -9,14 +9,14 @@ from motors.main import move_forward, turn_left, turn_right, stop
 from signal import signal, SIGINT
 from time import time
 
-# List of avaiable colors (use color name from CSS)
+# List of available colors (use color name from CSS)
 """COLORS = [name_to_rgb('red'),name_to_rgb('orange'),name_to_rgb('yellow'),name_to_rgb('green'),name_to_rgb('blue'),\
 	name_to_rgb('purple'),name_to_rgb('black'),name_to_rgb('brown'),name_to_rgb('white')]"""
 COLORS = [name_to_rgb("black"), name_to_rgb("white")]
 FSPEED = 20 # forward speed
 TSPEED = 30 # turn speed
 
-def stop_robot(signum, frame):
+def stop_robot():
 	# Stop the motors upon SIGINT
 	print("Stopping the print job...")
 	stop()
@@ -26,13 +26,13 @@ def closest_color(color):
 	# Find the color a pixel closely matches to
 	res = "???"
 	min_dist = sys.maxsize
-	r1, g1, b1, *a1 = color # might get rgba values, in that case extract the *a* values
+	r1, g1, b1, *a1 = color # might get RGBA values, in that case extract the *A* values
 
 	for ref_c in COLORS:
 		r2, g2, b2, *a2 = ref_c
 		dist = sqrt((r1-r2)**2 + (g1-g2)**2 + (b1-b2)**2)
 
-		if (dist < min_dist):
+		if dist < min_dist:
 			min_dist = dist
 			res = rgb_to_name(ref_c)
 
@@ -52,29 +52,29 @@ def check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color):
 
 def check_progress(progress):
 	# Show progress
-	if (progress == int(round(total*0.1))):
+	if progress == int(round(total*0.1)):
 		print("10% done", flush=True)
-	elif (progress == int(round(total*0.2))):
+	elif progress == int(round(total*0.2)):
 		print("20% done", flush=True)
-	elif (progress == int(round(total*0.3))):
+	elif progress == int(round(total*0.3)):
 		print("30% done", flush=True)
-	elif (progress == int(round(total*0.4))):
+	elif progress == int(round(total*0.4)):
 		print("40% done", flush=True)
-	elif (progress == int(round(total*0.5))):
+	elif progress == int(round(total*0.5)):
 		print("50% done", flush=True)
-	elif (progress == int(round(total*0.6))):
+	elif progress == int(round(total*0.6)):
 		print("60% done", flush=True)
-	elif (progress == int(round(total*0.7))):
+	elif progress == int(round(total*0.7)):
 		print("70% done", flush=True)
-	elif (progress == int(round(total*0.8))):
+	elif progress == int(round(total*0.8)):
 		print("80% done", flush=True)
-	elif (progress == int(round(total*0.9))):
+	elif progress == int(round(total*0.9)):
 		print("90% done", flush=True)
 
 argc = len(sys.argv)
-if (argc == 1):
+if argc == 1:
 	raise IOError("Please supply an image file.")
-elif (argc > 2):
+elif argc > 2:
 	raise IOError("Only 1 image file please. Thank you!")
 
 file = sys.argv[1]
@@ -104,6 +104,14 @@ stop()
 TL = time() - start_time
 input(f"Turning left for {TL} seconds, press enter to continue...")
 
+# Determine how far to move each cycle (s/px)
+w_dist = 30 # cm wide of printing area
+pix_width = w_dist/width # cm/px wide
+TFW = pix_width/FSPEED # time forward width (s/px)
+h_dist = 30 # cm tall of printing area
+pix_height = h_dist/height # cm/px tall
+TFH = pix_height/FSPEED # time forward height (s/px)
+
 print("Starting print job...", flush=True)
 # Print the first pixel
 prev_color = "white" # surface color
@@ -111,50 +119,50 @@ check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color) # really check c
 
 while x < width and y < height:
 	# Check state of robot
-	if (state == "left"):
+	if state == "left":
 		# Robot is moving left; if at the edge, need to turn left and move down
-		if (x == 0):
+		if x == 0:
 			state = "down"
 			y += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Turn left, move down")
 			turn_left(TSPEED, TL)
-			move_forward(FSPEED, 1)
+			move_forward(FSPEED, TFH)
 		else:
 			x -= 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Continue left")
-			move_forward(FSPEED, 1)
-	elif (state == "right"):
+			move_forward(FSPEED, TFW)
+	elif state == "right":
 		# Robot is moving right; if at the edge, need to turn right and move down
-		if (x == width - 1):
+		if x == width - 1:
 			state = "down"
 			y += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Turn right, move down")
 			turn_right(TSPEED, TR)
-			move_forward(FSPEED, 1)
+			move_forward(FSPEED, TFH)
 		else:
 			x += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Continue right")
-			move_forward(FSPEED, 1)
+			move_forward(FSPEED, TFW)
 	else:
 		# Robot moved down, need to turn in the right direction
-		if (x == 0):
+		if x == 0:
 			state = "right"
 			x += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Turn left, move right")
 			turn_left(TSPEED, TL)
-			move_forward(FSPEED, 1)
+			move_forward(FSPEED, TFW)
 		else:
 			state = "left"
 			x -= 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Turn right, move left")
 			turn_right(TSPEED, TR)
-			move_forward(FSPEED, 1)
+			move_forward(FSPEED, TFW)
 
 	progress += 1
 	check_progress(progress)
