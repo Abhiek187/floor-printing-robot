4d993bf083e9728014d050869dbaf21285bc1cd4
Abhiek187 <achaudhuri2011@yahoo.com>
2020-03-17 15:15:42 -0400

    Perform autonomous calibration

--- img_info.py
+++ img_info.py
@@ -6,7 +6,7 @@ from PIL import Image
 from math import sqrt
 from webcolors import name_to_rgb, rgb_to_name
 from motors.main import move_forward, turn_left, turn_right, stop
-from motors.ultrasonic import get_distance
+from motors.ultrasonic import get_distance, get_dimensions
 from signal import signal, SIGINT
 from time import time
 from threading import Thread
@@ -91,7 +91,7 @@ state = "right" # robot starts at top left moving right; possible states: left, 
 signal(SIGINT, stop_robot) # stop in case of an emergency
 
 # Perform calibration to determine turn speeds
-input("Calibration is needed to turn the robot 90\u00B0. Press enter to continue...") # let the user read the message
+"""input("Calibration is needed to turn the robot 90\u00B0. Press enter to continue...") # let the user read the message
 start_time = time()
 turn_right(TSPEED)
 input("Turning right, press enter to continue...")
@@ -104,14 +104,15 @@ turn_left(TSPEED)
 input("Turning left, press enter to continue...")
 stop()
 TL = time() - start_time
-input(f"Turning left for {TL} seconds, press enter to continue...")
+input(f"Turning left for {TL} seconds, press enter to continue...")"""
+TR, TL, w_dist, h_dist = get_dimensions(TSPEED)
 
 # Determine how far to move each cycle (s/px)
-w_dist = 14 # cm wide of printing area
+#w_dist = 14 # cm wide of printing area
 pix_width = w_dist/width # cm/px wide
 real_speed = 0.8*FSPEED - 6
 TFW = pix_width/real_speed # time forward width (s/px)
-h_dist = 20 # cm tall of printing area
+#h_dist = 20 # cm tall of printing area
 pix_height = h_dist/height # cm/px tall
 TFH = pix_height/real_speed # time forward height (s/px)
 
--- motors/ultrasonic.py
+++ motors/ultrasonic.py
@@ -3,7 +3,7 @@
 # Import required modules
 from time import sleep, time
 import RPi.GPIO as GPIO
-from main import stop
+from main import stop, turn_right, turn_left
 from atexit import register
 from sys import exit
 
@@ -30,6 +30,75 @@ def stop_ultrasonic_sensor():
     stop()
     GPIO.cleanup()
 
+def _get_distance():
+    # Secret distance function that returns a value
+    # Set Trigger to Low
+    GPIO.output(pins.Trigger, GPIO.LOW)
+
+    # Sensor calibration (let it settle for a second, sleep mode...:())
+    sleep(1)
+
+    # Set Trigger to High for 10 us
+    GPIO.output(pins.Trigger, GPIO.HIGH)
+    sleep(0.00001)
+    GPIO.output(pins.Trigger, GPIO.LOW)
+
+    # Condition to set start/stop time based on echo (timeout if taking too long)
+    print("Echo = 0")
+    pulse_start = time()
+    max_time = pulse_start + timeout
+    while GPIO.input(pins.Echo) == 0 and pulse_start < max_time:
+        pulse_start = time()
+
+    if GPIO.input(pins.Echo) == 0:
+        return -1 # device is broken
+
+    print("Echo = 1")
+    pulse_end = time()
+    max_time = pulse_end + timeout
+    while GPIO.input(pins.Echo) == 1 and pulse_end < max_time:
+        pulse_end = time()
+
+    if GPIO.input(pins.Echo) == 1:
+        return -1 # device is broken
+
+    # Calculate distance: take 34300 cm/s and divide it by 2 for the one-way trip
+    pulse_duration = pulse_end - pulse_start
+    # Round to 2 decimal places
+    return round(pulse_duration * 17150, 2)
+
+def get_dimensions(turn_speed):
+    # Start by turning right and looking for a marker
+    start_time = time()
+    turn_right(turn_speed)
+    height = _get_distance()
+
+    while height < 50:
+        # 50 cm limit on tracking distance
+        height = _get_distance()
+
+    stop()
+    right_time = time() - start_time
+
+    if height == -1:
+        raise RuntimeError("The ultrasonic sensors are not working.")
+
+    # Then turn left and look for the other marker
+    start_time = time()
+    turn_left(turn_speed)
+    width = _get_distance()
+
+    while width < 50:
+        width = _get_distance()
+
+    stop()
+    left_time = time() - start_time
+
+    if width == -1:
+        raise RuntimeError("The ultrasonic sensors are not working.")
+
+    return (right_time, left_time, width, height)
+
 def get_distance():
     while count > 0:
         # Set Trigger to Low
@@ -38,7 +107,7 @@ def get_distance():
         # Sensor calibration (let it settle for a second, sleep mode...:())
         sleep(1)
 
-        # Set Trigger to High
+        # Set Trigger to High for 10 us
         GPIO.output(pins.Trigger, GPIO.HIGH)
         sleep(0.00001)
         GPIO.output(pins.Trigger, GPIO.LOW)
