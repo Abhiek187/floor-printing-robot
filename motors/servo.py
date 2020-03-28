6bd52a6f7b1947037eb6d34d0b4b45f03cb58ed0
Abhiek187 <achaudhuri2011@yahoo.com>
2020-03-28 17:23:07 -0400

    Test servo at precise angles

--- motors/servo.py
+++ motors/servo.py
@@ -24,12 +24,25 @@ pins = Pin()
 GPIO.setup(pins.PWM, GPIO.OUT)
 servo = GPIO.PWM(pins.PWM, 50) # freq = 50 Hz
 servo.start(0) # 0% <= duty cycle <= 100%
-sleep(2)
 
 try:
 	while True:
 		# Turn 180 degrees in 10 steps (2% = 0d, 12% = 180d, deg = 18*dc - 36, dc = deg/18 + 2)
-		dc = 2
+		angle = -1
+
+		# Do robust error checking (like a good boi)
+		while angle < 0 or angle > 180:
+			try:
+				angle = float(input("Enter angle between 0 and 180: "))
+				if angle < 0 or angle > 180:
+					print("Angle is not within bounds, try again.")
+			except ValueError:
+				print("That's not a number, try again.")
+
+		servo.ChangeDutyCycle(angle/18 + 2)
+		sleep(0.3)
+		servo.ChangeDutyCycle(0) # by the time user inputs, a second has gone by
+		"""dc = 2
 
 		while dc <= 12:
 			servo.ChangeDutyCycle(dc)
@@ -48,7 +61,7 @@ try:
 		servo.ChangeDutyCycle(2)
 		sleep(0.5)
 		servo.ChangeDutyCycle(0)
-		sleep(1.5)
+		sleep(1.5)"""
 except KeyboardInterrupt:
 	print("Stopping the servo...")
 	servo.stop()
