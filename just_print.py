d5a449ec3afa0c8852628ac6cac48d131dc0fabe
Abhiek187 <achaudhuri2011@yahoo.com>
2020-04-01 17:06:53 -0400

    Add syntax improvements from before

--- just_print.py
+++ just_print.py
@@ -4,7 +4,7 @@ from PIL import Image
 from math import sqrt
 from webcolors import name_to_rgb, rgb_to_name
 
-# List of avaiable colors (use color name from CSS)
+# List of available colors (use color name from CSS)
 """COLORS = [name_to_rgb('red'),name_to_rgb('orange'),name_to_rgb('yellow'),name_to_rgb('green'),name_to_rgb('blue'),\
 	name_to_rgb('purple'),name_to_rgb('black'),name_to_rgb('brown'),name_to_rgb('white')]"""
 COLORS = [name_to_rgb("black"), name_to_rgb("white")]
@@ -13,13 +13,13 @@ def closest_color(color):
 	# Find the color a pixel closely matches to
 	res = "???"
 	min_dist = sys.maxsize
-	r1, g1, b1, *a1 = color # might get rgba values, in that case extract the *a* values
+	r1, g1, b1, *a1 = color # might get RGBA values, in that case extract the *A* values
 
 	for ref_c in COLORS:
 		r2, g2, b2, *a2 = ref_c
 		dist = sqrt((r1-r2)**2 + (g1-g2)**2 + (b1-b2)**2)
 
-		if (dist < min_dist):
+		if dist < min_dist:
 			min_dist = dist
 			res = rgb_to_name(ref_c)
 
@@ -39,29 +39,29 @@ def check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color):
 
 def check_progress(progress):
 	# Show progress
-	if (progress == int(round(total*0.1))):
+	if progress == int(round(total*0.1)):
 		print("10% done", flush=True)
-	elif (progress == int(round(total*0.2))):
+	elif progress == int(round(total*0.2)):
 		print("20% done", flush=True)
-	elif (progress == int(round(total*0.3))):
+	elif progress == int(round(total*0.3)):
 		print("30% done", flush=True)
-	elif (progress == int(round(total*0.4))):
+	elif progress == int(round(total*0.4)):
 		print("40% done", flush=True)
-	elif (progress == int(round(total*0.5))):
+	elif progress == int(round(total*0.5)):
 		print("50% done", flush=True)
-	elif (progress == int(round(total*0.6))):
+	elif progress == int(round(total*0.6)):
 		print("60% done", flush=True)
-	elif (progress == int(round(total*0.7))):
+	elif progress == int(round(total*0.7)):
 		print("70% done", flush=True)
-	elif (progress == int(round(total*0.8))):
+	elif progress == int(round(total*0.8)):
 		print("80% done", flush=True)
-	elif (progress == int(round(total*0.9))):
+	elif progress == int(round(total*0.9)):
 		print("90% done", flush=True)
 
 argc = len(sys.argv)
-if (argc == 1):
+if argc == 1:
 	raise IOError("Please supply an image file.")
-elif (argc > 2):
+elif argc > 2:
 	raise IOError("Only 1 image file please. Thank you!")
 
 file = sys.argv[1]
@@ -81,9 +81,9 @@ check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color) # really check c
 
 while x < width and y < height:
 	# Check state of robot
-	if (state == "left"):
+	if state == "left":
 		# Robot is moving left; if at the edge, need to turn left and move down
-		if (x == 0):
+		if x == 0:
 			state = "down"
 			y += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
@@ -92,9 +92,9 @@ while x < width and y < height:
 			x -= 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
 			#print("Continue left")
-	elif (state == "right"):
+	elif state == "right":
 		# Robot is moving right; if at the edge, need to turn right and move down
-		if (x == width - 1):
+		if x == width - 1:
 			state = "down"
 			y += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
@@ -105,7 +105,7 @@ while x < width and y < height:
 			#print("Continue right")
 	else:
 		# Robot moved down, need to turn in the right direction
-		if (x == 0):
+		if x == 0:
 			state = "right"
 			x += 1
 			check_next_pixel(x, y, width, height, pix, rgb_arr, prev_color)
